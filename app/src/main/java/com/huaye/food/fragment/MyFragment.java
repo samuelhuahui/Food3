07892c7d3a3e852bd109aa86d9c3602c72213225
sunhuahui <sunhuahui@ppdai.com>
2017-10-16 13:21:39 +0800

    隐藏健康记步

--- app/src/main/java/com/huaye/food/fragment/MyFragment.java
+++ app/src/main/java/com/huaye/food/fragment/MyFragment.java
@@ -16,7 +16,6 @@ import android.widget.Toast;
 import com.huaye.food.R;
 import com.huaye.food.WebViewActivity;
 
-import cn.bluemobi.dylan.step.activity.StepActivity;
 import cn.bmob.v3.BmobUser;
 
 /**
@@ -47,7 +46,7 @@ public class MyFragment extends Fragment {
         step.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getContext(), StepActivity.class));
+//                startActivity(new Intent(getContext(), StepActivity.class));
             }
         });
 
